5a820c868fdbce2b81b9e42909d59891266807fe
MingzeDong <68533876+MingzeDong@users.noreply.github.com>
2022-04-10 00:42:59 -0400

    Update __init__.py

--- cinemaot/__init__.py
+++ cinemaot/__init__.py
@@ -1,3 +1,3 @@
 """CINEMA-OT - Causal Independent Effect Module Attribution + Optimal Transport, for single-cell level treatment effect identification"""
 __version__ = "0.1.0"
-from . import cinemaot
+from . import cinemaot, utils
